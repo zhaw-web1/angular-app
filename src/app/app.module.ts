e92048bab06a77476c1189d25f538e30761c8988
cfi <carlo@field.li>
2018-11-11 16:06:31 +0100

    add firebase to app
    Signed-off-by: cfi <carlo@field.li>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ComponentModule} from './component/component.module';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import {ComponentModule} from './component/component.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentModule
+    ComponentModule,
+    AngularFireModule.initializeApp(environment.firebase, 'Scythe of Seraph')
   ],
   providers: [],
   bootstrap: [AppComponent]
