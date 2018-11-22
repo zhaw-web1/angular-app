009b917fe746d08fb4e4f97385bd19aabfc0a53d
Denis Müller <denismueller.private@gmail.com>
2018-11-22 16:26:12 +0100

    refactor match class structure

--- src/app/component/match/match.model.ts
+++ src/app/component/match/match.model.ts
@@ -1,17 +1,11 @@
-import {Team} from './team.model';
 import Timestamp = firebase.firestore.Timestamp;
 import * as firebase from 'firebase';
+import {TeamStats} from './team-stats.model';
 
 export class Match {
   // Firebase ids can be strings, they just need to be unique.
   // This can be changed if we want to switch to a different type of db system.
   id: string;
-
-  team1: Team;
-  team1score: number;
-
-  team2: Team;
-  team2score: number;
-
+  stats: [TeamStats, TeamStats];
   date: Timestamp;
 }
