bb9fe3e4a6df4c55e9e35f4072c3d4baa9597a1e
Denis Müller <denismueller.private@gmail.com>
2018-11-25 10:34:26 +0100

    Display basic data in match detail component

--- src/app/component/match/detail/match-detail.component.ts
+++ src/app/component/match/detail/match-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Match} from '../match.model';
 
 @Component({
   selector: 'app-match-detail',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./match-detail.component.scss']
 })
 export class MatchDetailComponent implements OnInit {
+  @Input() match: Match;
 
   constructor() { }
 
