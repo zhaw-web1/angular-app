77b8893e3c9a6752429b260137e465234b1a1190
zischler <tobias.zischler@gmx.ch>
2018-12-01 21:37:28 +0100

    - added team-person - added basic flex styling to team

---
+++ src/app/component/teams/team-detail/team-person/team-person.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-person',
+  templateUrl: './team-person.component.html',
+  styleUrls: ['./team-person.component.scss']
+})
+export class TeamPersonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/component/teams/teams.module.ts
+++ src/app/component/teams/teams.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { TeamOverviewComponent } from './team-overview/team-overview.component';
 import { TeamDetailComponent } from './team-detail/team-detail.component';
 import {TeamsRoutingModule} from './teams-routing.module';
+import { TeamPersonComponent } from './team-detail/team-person/team-person.component';
 
 @NgModule({
-  declarations: [TeamOverviewComponent, TeamDetailComponent],
+  declarations: [TeamOverviewComponent, TeamDetailComponent, TeamPersonComponent],
   imports: [
     CommonModule,
     TeamsRoutingModule
