f317285ce7a03fa334394288e3883dbc4266dda7
cfi <carlo@field.li>
2018-12-02 13:43:57 +0100

    add home sections

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {HeaderService} from '../header/header.service';
+import {NewsService} from '../news/news.service';
+import {MatchService} from '../match/match.service';
+import {Match} from '../match/match.model';
+import {Observable} from 'rxjs';
+import {News} from '../news';
 
 @Component({
   selector: 'app-home',
@@ -7,14 +12,23 @@ import {HeaderService} from '../header/header.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private matches: Observable<Match[]>;
+  private news: Observable<News[]>;
 
-  constructor(private header: HeaderService) { }
+  constructor(
+    private header: HeaderService,
+    private newsService: NewsService,
+    private matchService: MatchService
+  ) { }
 
   ngOnInit() {
     this.header.setTitle('Scythe of Seraph');
     this.header
       .setImage(
         '/assets/img/banners/caspar-rubin-680338-unsplash.jpg');
+
+    this.matches = this.matchService.getNewestMatches(3);
+    this.news = this.newsService.getNewestArticles(3);
   }
 
 }
