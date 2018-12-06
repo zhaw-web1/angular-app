c4346397657adacc47a36c72f16c9f904894eb43
cfi <carlo@field.li>
2018-12-06 18:01:18 +0100

    add /admin/match route

--- src/app/component/admin/match-admin/match-admin-overview/match-admin-overview.component.ts
+++ src/app/component/admin/match-admin/match-admin-overview/match-admin-overview.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {MatchService} from '../../../match/match.service';
 import {Match} from '../../../match/models/match.model';
 import {Observable} from 'rxjs';
+import {HeaderService} from '../../../header/header.service';
 
 @Component({
   selector: 'app-match-admin-overview',
@@ -13,11 +14,14 @@ export class MatchAdminOverviewComponent implements OnInit {
   matches: Observable<Match[]>;
 
   constructor(
-    private matchService: MatchService
+    private matchService: MatchService,
+    private header: HeaderService
   ) { }
 
   ngOnInit() {
     this.matches = this.matchService.getMatches();
+    this.header.setTitle('Admin - Matches');
+    this.header.setImage('/assets/img/banners/desktop-header.png');
   }
 
 }
--- src/app/component/admin/match-admin/match-admin-routing.module.ts
+++ src/app/component/admin/match-admin/match-admin-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MatchAdminEditComponent} from './match-admin-edit/match-admin-edit.component';
+import {MatchAdminOverviewComponent} from './match-admin-overview/match-admin-overview.component';
 const routes: Routes = [
+  {
+    path: '',
+    component: MatchAdminOverviewComponent
+  },
   {
     path: ':match',
     component: MatchAdminEditComponent
