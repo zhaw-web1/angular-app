a5e9082948ca7bd3ab6eb1f63ca4ae390879ec22
cfi <carlo@field.li>
2018-12-10 19:20:31 +0100

    add content page admin edit component

--- src/app/component/admin/content-page-admin/content-page-admin-routing.module.ts
+++ src/app/component/admin/content-page-admin/content-page-admin-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {OverviewComponent} from './overview/overview.component';
+import {EditComponent} from './edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OverviewComponent
+  },
+  {
+    path: ':page',
+    component: EditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/component/admin/content-page-admin/content-page-admin.module.ts
+++ src/app/component/admin/content-page-admin/content-page-admin.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ContentPageAdminRoutingModule } from './content-page-admin-routing.module';
 import { OverviewComponent } from './overview/overview.component';
 import { FormComponent } from './form/form.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
-  declarations: [OverviewComponent, FormComponent],
+  declarations: [OverviewComponent, FormComponent, EditComponent],
   imports: [
     CommonModule,
     ContentPageAdminRoutingModule
