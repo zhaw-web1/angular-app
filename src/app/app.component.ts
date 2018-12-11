bfb146f69e3b1cee82471d766f7f509f02770e6c
cfi <carlo@field.li>
2018-12-11 21:39:32 +0100

    logged in user admin check and noscript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './core/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'webapp';
+export class AppComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService
+  ) {
+  }
+
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn().subscribe(loggedIn => {
+      if (loggedIn) {
+        this.authService.isAdmin().subscribe(admin => {
+          if (!admin) {
+            this.authService.logout();
+          }
+        });
+      }
+    });
+  }
+
 }
