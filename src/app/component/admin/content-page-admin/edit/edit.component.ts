df7d98148bcc0d0bf3b8739041f7a70d001eb363
cfi <carlo@field.li>
2018-12-11 15:37:00 +0100

    add form functions

--- src/app/component/admin/content-page-admin/edit/edit.component.ts
+++ src/app/component/admin/content-page-admin/edit/edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ContentService} from '../../../content-page/content.service';
+import {Observable} from 'rxjs';
+import {Page} from '../../../content-page/page.model';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  page: Page;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private contentService: ContentService
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('page');
+    this.contentService.getPage(id).subscribe(page => this.page = page);
+  }
+
+  submit() {
+    this.contentService.updatePage(this.page, this.page.id);
   }
 
 }
--- src/app/component/admin/content-page-admin/form/form.component.ts
+++ src/app/component/admin/content-page-admin/form/form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Page} from '../../../content-page/page.model';
+import {Content, Image, Paragraph, Title} from '../../../content-page';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  @Input()
+  page: Page = {} as Page;
+
+  @Output()
+  submit: EventEmitter<Page> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  _submit() {
+    this.submit.emit(this.page);
+  }
+
+  addContent(id: number, type: string) {
+    this.page.content[id] = this.getContentForType(type);
+    this._submit();
+  }
+
+  getContentForType(type: string): Content {
+    if (type === 'title') return this.getBlankTitle();
+    else if (type === 'paragraph') return this.getBlankParagraph();
+    else if (type === 'image') return this.getBlankImage();
+    else return null;
+  }
+
+  getBlankTitle(): Title {
+    return {
+      type: 'title',
+      title: ''
+    } as Title;
+  }
+
+  getBlankParagraph(): Paragraph {
+    return {
+      type: 'paragraph',
+      text: '',
+      title: {}
+    } as Paragraph;
+  }
+
+  getBlankImage(): Image {
+    return {
+      type: 'image',
+      image: ''
+    } as Image;
   }
 
 }
