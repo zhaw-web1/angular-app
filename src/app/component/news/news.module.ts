ed34da60dfdc81114bff90ef0a15864814237504
cfi <carlo@field.li>
2018-12-13 14:11:11 +0100

    remove obsolete news title component

--- src/app/component/news/news.module.ts
+++ src/app/component/news/news.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NewsPreviewComponent} from './preview/news-preview.component';
-import {NewsTitleComponent} from './title/news-title.component';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {NewsOverviewComponent} from './overview/news-overview.component';
 import {NewsRoutingModule} from './news-routing.module';
@@ -14,7 +13,6 @@ import {NewsRoutingModule} from './news-routing.module';
   ],
   declarations: [
     NewsPreviewComponent,
-    NewsTitleComponent,
     NewsOverviewComponent],
   exports: [NewsPreviewComponent]
 })
