2df06c85481a538f6dd3337a6a3a103449778240
zischler <tobias.zischler@gmx.ch>
2019-01-10 18:43:17 +0100

    - fixed footer nav component - added team back arrow

--- src/app/component/teams/teams.module.ts
+++ src/app/component/teams/teams.module.ts
@@ -4,9 +4,10 @@ import { TeamOverviewComponent } from './team-overview/team-overview.component';
 import { TeamDetailComponent } from './team-detail/team-detail.component';
 import {TeamsRoutingModule} from './teams-routing.module';
 import {PersonModule} from '../person/person.module';
+import { TeamBackArrowComponent } from './team-back-arrow/team-back-arrow.component';
 
 @NgModule({
-  declarations: [TeamOverviewComponent, TeamDetailComponent],
+  declarations: [TeamOverviewComponent, TeamDetailComponent, TeamBackArrowComponent],
   imports: [
     CommonModule,
     TeamsRoutingModule,
