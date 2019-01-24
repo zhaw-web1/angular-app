0dc77d1616bcb24cc8926a8336e80f7b89b28e7c
cfi <carlo@field.li>
2019-01-24 14:24:07 +0100

    non authorized user redirect to login

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,7 +1,8 @@
 import {Injectable, OnInit} from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from './auth.service';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,17 @@ import {AuthService} from './auth.service';
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {
 
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.authService.isAuthorizedUser();
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.isAuthorizedUser().pipe(
+      map(authorized => authorized ? authorized : this.router.parseUrl('/login'))
+    );
   }
 }
