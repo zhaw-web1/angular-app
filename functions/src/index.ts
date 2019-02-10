b524ce95ca9ca73e1276b564ef75e323aa9b5780
cfi <carlo@field.li>
2019-02-10 15:47:34 +0100

    add firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,21 @@
+import {https} from 'firebase-functions';
+import * as express from 'express';
+import * as cors from 'cors';
+import {ContactApp} from './apps';
+import * as bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors({origin: '*'}));
+app.use(bodyParser());
+
+app.post('/', ContactApp);
+
+export const contact = https.onRequest(app);
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
