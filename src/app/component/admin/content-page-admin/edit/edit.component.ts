09401e5dc08a090dc23ac2df51b68b091cca3d9d
cfi <carlo@field.li>
2019-03-20 19:35:09 +0100

    update tests

--- src/app/component/admin/content-page-admin/edit/edit.component.ts
+++ src/app/component/admin/content-page-admin/edit/edit.component.ts
@@ -4,6 +4,7 @@ import {ContentService} from '../../../content-page/content.service';
 import {Observable} from 'rxjs';
 import {Page} from '../../../content-page/page.model';
 import {HeaderService} from '../../../header/header.service';
+import {AngularFireStorage} from '@angular/fire/storage';
 
 @Component({
   selector: 'app-edit',
@@ -16,7 +17,8 @@ export class EditComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private contentService: ContentService,
-    private header: HeaderService
+    private header: HeaderService,
+    private storage: AngularFireStorage
   ) { }
 
   ngOnInit() {
@@ -24,7 +26,13 @@ export class EditComponent implements OnInit {
     this.header.setImage('https://firebasestorage.googleapis.com/v0/b/scythe-of-seraph-e7412.appspot.com/o/header-images%2F' +
     'desktop-header.jpg?alt=media&token=560a02d4-e69e-44bd-8677-14e649174c5f');
     const id = this.route.snapshot.paramMap.get('page');
-    this.contentService.getPage(id).subscribe(page => this.page = page);
+    this.contentService.getPage(id).subscribe(page => {
+      this.page = page;
+      if (page.usesNewImage) {
+        this.storage
+          .ref(`content-page/images/${page.id}/image@1920`).getDownloadURL().subscribe(r => this.header.setImage(r));
+      }
+    });
   }
 
   submit() {
