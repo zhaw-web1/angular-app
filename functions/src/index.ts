d819c782ed2701b1311809513a48ec6ed26db921
zischler <tobias.zischler@gmx.ch>
2019-08-15 18:06:17 +0200

    - added code from tutorial to make 404 service work. Don't know since I couldn't deploy onto firebase.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,18 @@
 import {https} from 'firebase-functions';
 import * as express from 'express';
 import * as cors from 'cors';
-import {ContactApp, NotFoundApp, thumbnailGenerator} from './apps';
+import {ContactApp, thumbnailGenerator} from './apps';
 import * as bodyParser from 'body-parser';
+
+//import { ngExpressEngine } from '@nguniversal/express-engine';
+
+import { renderModuleFactory } from '@angular/platform-server';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
+import {template} from '@angular/core/src/render3';
+//import { ValueProvider } from '@angular/core';
+
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/webapp/main.bundle');
 
 const app = express();
 
@@ -15,14 +25,27 @@ export const contact = https.onRequest(app);
 
 export const thumbnails = thumbnailGenerator;
 
-const notFoundApp = express();
+app.engine('html', (_, options, callback) => {
+  const opts = {
+    document: template,
+    url: options.req.url,
+    extraProviders: [
+      provideModuleMap(LAZY_MODULE_MAP),
+      {
+        provide: REQUEST,
+        useValue: options.req
+      },
+      {
+        provide: RESPONSE,
+        useValue: options.req.res,
+      },
+    ]
+  };
 
-notFoundApp.use(cors({origin: '*'}));
-
-notFoundApp.post('', bodyParser(), NotFoundApp);
-notFoundApp.options('', cors({origin: '*'}));
-
-export const notFound = https.onRequest(notFoundApp);
+renderModuleFactory(AppServerModuleNgFactory, opts)
+  .then(html => callback(null, html)
+);
+});
 
 
 // // Start writing Firebase Functions
--- src/app/component/not-found/not-found.component.ts
+++ src/app/component/not-found/not-found.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit, Optional} from '@angular/core';
 import {HeaderService} from '../header/header.service';
+import { RESPONSE } from '@nguniversal/express-engine/tokens';
 
 @Component({
   selector: 'app-not-found',
@@ -8,11 +9,10 @@ import {HeaderService} from '../header/header.service';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor(
-    private headerService: HeaderService
-  ) { }
-
+  constructor( private headerService: HeaderService, @Optional() @Inject(RESPONSE) private response: any) { }
   ngOnInit() {
+    this.response.statusCode = 404;
+    this.response.statusMessage = '404 - Page Not Found';
     this.headerService.setTitle('404');
     this.headerService.setImage('https://firebasestorage.googleapis.com/v0/b/scythe-of-seraph-e7412.appspot.com/o/header-images%2F' +
     'desktop-header.jpg?alt=media&token=560a02d4-e69e-44bd-8677-14e649174c5f');
