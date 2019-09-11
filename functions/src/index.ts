1c2354f8915d26d24ca5d181f5e7a4153a40983e
zischler <tobias.zischler@gmx.ch>
2019-09-11 14:46:20 +0200

    - added some more server side rendering code. Still has errors so starting all over in new branch.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,13 +6,7 @@ import * as bodyParser from 'body-parser';
 
 //import { ngExpressEngine } from '@nguniversal/express-engine';
 
-import { renderModuleFactory } from '@angular/platform-server';
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
-import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
-import {template} from '@angular/core/src/render3';
 //import { ValueProvider } from '@angular/core';
-
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/webapp/main.bundle');
 
 const app = express();
 
@@ -24,28 +18,6 @@ app.options('', cors({origin: '*'}));
 export const contact = https.onRequest(app);
 
 export const thumbnails = thumbnailGenerator;
-
-app.engine('html', (_, options, callback) => {
-  const opts = {
-    document: template,
-    url: options.req.url,
-    extraProviders: [
-      provideModuleMap(LAZY_MODULE_MAP),
-      {
-        provide: REQUEST,
-        useValue: options.req
-      },
-      {
-        provide: RESPONSE,
-        useValue: options.req.res,
-      },
-    ]
-  };
-
-renderModuleFactory(AppServerModuleNgFactory, opts)
-  .then(html => callback(null, html)
-);
-});
 
 
 // // Start writing Firebase Functions
