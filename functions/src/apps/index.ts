e513356166d4a37195d7629aa1838a4b8441b4ef
zischler <tobias.zischler@gmx.ch>
2019-09-11 15:40:38 +0200

    - removed not-found app test to start over

--- functions/src/apps/index.ts
+++ functions/src/apps/index.ts
@@ -1,3 +1,2 @@
 export * from './contact';
 export * from './image';
-export * from './not-found';
--- functions/src/apps/not-found.ts
+++
@@ -1,7 +0,0 @@
-import {Request, Response} from 'express';
-import {config} from 'firebase-functions';
-
-export async function NotFoundApp(req: Request, res: Response) {
-  res.status(404).sendFile('../static-pages/not-found.html');
-  return;
-}
